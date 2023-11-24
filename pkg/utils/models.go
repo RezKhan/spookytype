be2073288535ccc830cce126722638b3f05a51fc
Rez <probablyrez@gmail.com>
2023-11-24 20:40:44 +1100

    3 queries instead of 1, but it works better

--- pkg/utils/models.go
+++ pkg/utils/models.go
@@ -19,53 +19,64 @@ type Paragraph struct {
 }
 
 func GetParagraph() Paragraph {
-	rand.New(rand.NewSource(time.Now().UnixNano()))
-	// TODO: Fix the typing difficulty and make sure there's a minimum rating of 1
-	rnum := rand.Intn(4)
-	if rnum <= 1 {
-		rnum = 2
-	}
-
 	db, err := sql.Open("sqlite3", "books.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	// SELECT paragraphs.text, paragraphs.id, books.title, authors.name
-	// FROM paragraphs
-	// INNER JOIN books ON books.id = paragraphs.book_id
-	// INNER JOIN authors on authors.id = books.author_id
-	// WHERE paragraphs.difficulty = ?
+	minmaxrow := db.QueryRow("SELECT MIN(rating), MAX(rating) FROM paragraphs")
+	minrating := 0
+	maxrating := 0
+	level := 1
+	minmaxrow.Scan(&minrating, &maxrating)
 
-	rows, err := db.Query("SELECT paragraphs.text, paragraphs.id, books.title, authors.name "+
-		"FROM paragraphs "+
-		"INNER JOIN books ON books.id = paragraphs.book_id "+
-		"INNER JOIN authors ON authors.id = books.author_id "+
-		"WHERE paragraphs.difficulty = ?", rnum)
+	subrange := (maxrating - minrating + 1) / 25
+
+	idrows, err := db.Query("SELECT id FROM paragraphs WHERE rating BETWEEN ? AND ?", 
+		minrating, minrating + (subrange * level) - 1)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer rows.Close()
-
-	var paragraphs []Paragraph
-	for rows.Next() {
-		var paragraph Paragraph
-		var ptext string
-
-		err = rows.Scan(&ptext, &paragraph.ID, &paragraph.BookTitle, &paragraph.Author)
+	
+	var pids []int
+	for idrows.Next() {
+		var pid int
+		err = idrows.Scan(&pid)
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
 		}
+		pids = append(pids, pid)
+	}
+	idrows.Close()
 
-		words := strings.Fields(ptext)
-		for _, word := range words {
-			chars := strings.Split(word, "")
-			paragraph.Chars = append(paragraph.Chars, chars)
-		}
-		paragraph.Text = strings.Split(ptext, "")
-		paragraphs = append(paragraphs, paragraph)
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+	rnum := rand.Intn(len(pids))
+
+	row := db.QueryRow("SELECT paragraphs.text, paragraphs.id, paragraphs.rating, books.title, authors.name "+ 
+		"FROM paragraphs "+
+		"INNER JOIN books ON books.id = paragraphs.book_id "+
+		"INNER JOIN authors ON authors.id = books.author_id "+
+		"WHERE paragraphs.id = ?", pids[rnum])
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	return paragraphs[rand.Intn(len(paragraphs))]
+	var paragraph Paragraph
+	var ptext string
+	var prating int
+
+	err = row.Scan(&ptext, &paragraph.ID, &prating, &paragraph.BookTitle, &paragraph.Author)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(paragraph.ID, prating)
+	words := strings.Fields(ptext)
+	for _, word := range words {
+		chars := strings.Split(word, "")
+		paragraph.Chars = append(paragraph.Chars, chars)
+	}
+	paragraph.Text = strings.Split(ptext, "")
+
+	return paragraph
 }
